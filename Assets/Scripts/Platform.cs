e4b08cced2b216d16f397ef7e5783268abc1650e
Lawrence1031 <pastelblue1031@gmail.com>
2024-06-04 01:33:43 +0900

    Add_platform
    플랫폼, 장애물, 점수 추가 구현

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] obstacles;
+    private bool stepped = false;
+
+    private void OnEnable()
     {
-        
+        stepped = false;
+
+        for (int i = 0; i < obstacles.Length; i++)
+        {
+            if (Random.Range(0, 3) == 0)
+            {
+                obstacles[i].SetActive(true);
+            }
+            else
+            {
+                obstacles[i].SetActive(false);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.collider.tag == "Player" && !stepped)
+        {
+            stepped = true;
+            GameManager.instance.AddScore(1);
+        }
     }
 }
